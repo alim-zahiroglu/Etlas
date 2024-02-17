d481f373f8a3634bf2ccc4f62a90b5f2f0d04ce8
ziya <koltikin210@gmail.com>
2024-02-18 01:21:52 +0300

    search button added dropdown export and drop down more action are ready

--- src/main/resources/static/data-table-assets/js/custom_data_tables.js
+++ src/main/resources/static/data-table-assets/js/custom_data_tables.js
@@ -1,73 +1,8 @@
-// $(document).ready(function () {
-//     // Initialize DataTable with Buttons
-//     const table = $('#example').DataTable({
-//         buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-//     });
-//
-//     // Move DataTable buttons container to a specific location
-//     table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
-//
-//     // Enable tooltip for elements with data-toggle="tooltip"
-//     $('[data-toggle="tooltip"]').tooltip();
-//
-//     // Filter table rows based on input value
-//     $("#myInput").on("keyup", function () {
-//         let value = $(this).val().toLowerCase();
-//         $("#example tr").filter(function () {
-//             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-//         });
-//     });
-// });
-
-
-// $(document).ready(function () {
-//     // Initialize DataTable with Buttons
-//     const table = $('#example').DataTable({
-//         buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-//     });
-//
-//     // Move DataTable buttons container to a specific location
-//     table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
-//
-//     // Manually handle Copy and Print buttons
-//     $('#copy').on('click', function () {
-//         table.button(0).trigger();
-//     });
-//
-//     $('#print').on('click', function () {
-//         table.button(4).trigger();
-//     });
-//
-//     // Manually handle CSV, Excel, and PDF buttons
-//     $('#export_pdf').on('click', function () {
-//         table.button(3).trigger();
-//     });
-//
-//     $('#export_Excel').on('click', function () {
-//         table.button(2).trigger();
-//     });
-//
-//     $('#export_CVS').on('click', function () {
-//         table.button(1).trigger();
-//     });
-//
-//     // Enable tooltip for elements with data-toggle="tooltip"
-//     $('[data-toggle="tooltip"]').tooltip();
-//
-//     // Filter table rows based on input value
-//     $("#myInput").on("keyup", function () {
-//         let value = $(this).val().toLowerCase();
-//         $("#example tr").filter(function () {
-//             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-//         });
-//     });
-// });
-
-
 $(document).ready(function () {
-    // Initialize DataTable with Buttons
+    // Initialize DataTable with Buttons and disable searching
     const table = $('#example').DataTable({
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+        buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
+        lengthMenu: [10, 25, 50, 75, 100]
     });
 
     // Move DataTable buttons container to a specific location
@@ -106,3 +41,5 @@ $(document).ready(function () {
         });
     });
 });
+
+
