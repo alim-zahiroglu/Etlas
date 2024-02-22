d75a54c2f89a1d8269a3e530f52c88e485c4dcc9
ziya <koltikin210@gmail.com>
2024-02-22 11:40:04 +0300

    base entity, base entity listener, data.sql, Role, user, userdto, UserPrincipal, UserStatus added

---
+++ src/main/java/com/etlas/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.etlas.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(BaseEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Boolean isDeleted = false;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
+    private Long insertUserId;
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
+    private Long lastUpdateUserId;
+
+
+}
