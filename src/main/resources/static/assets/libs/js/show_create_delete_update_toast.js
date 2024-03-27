2f028d8e56c079cb5c709165a4db9680e10f2546
ziya <koltikin210@gmail.com>
2024-03-28 00:07:23 +0300

    ticket list page customer details changed

--- src/main/resources/static/assets/libs/js/show_create_delete_update_toast.js
+++ src/main/resources/static/assets/libs/js/show_create_delete_update_toast.js
@@ -68,3 +68,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 2500);  // 2500 milliseconds = 2.5 seconds (time the toast is visible)
     }, 300);  // 300 milliseconds = 0.3 second (delay before showing the toast)
 });
+
+// showing customer successfully update message in ticket list
+document.addEventListener('DOMContentLoaded', function () {
+    const updateSuccessToast = document.getElementById('customer_updateSuccessToast');
+    setTimeout(function () {
+        updateSuccessToast.classList.add('show');
+
+        // Set timeout to hide the toast after 2 seconds
+        setTimeout(function () {
+            updateSuccessToast.classList.remove('show');
+
+        }, 2500);  // 2500 milliseconds = 2.5 seconds (time the toast is visible)
+    }, 300);  // 300 milliseconds = 0.3 second (delay before showing the toast)
+});
